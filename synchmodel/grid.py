9c970b169547af2e262ec76b9f5aa20b97791090
GCassCapellini <gabriel.capellini@usp.br>
2025-05-08 23:51:35 -0300

    Update grid.py

--- synchmodel/grid.py
+++ synchmodel/grid.py
@@ -5,6 +5,19 @@ import numpy as np
 #Os parâmetros são: min (float), max (float), itrcn (int)
 
 def grid(Min,Max,itrcn):
+    """
+    Creates a grid o parameters as a numpy.ndarray
+
+    Parameters:
+
+    Min (float): Minimum of the grid
+    Max (float): Maximum of the grid
+    itrcn (float): Number of spaces in the grid
+
+    Returns:
+
+    array (numpy.ndarray): Grid of values from Min to Max
+    """
     Min = Min #defined minimum if the grid
     Max = Max #defined maximum of the grid
     itrcn = itrcn #defined number of interaction  
