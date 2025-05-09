3336f7eeb354e687b0c002dbdc7cddae3e216478
GCassCapellini <gabriel.capellini@usp.br>
2025-05-08 23:50:51 -0300

    Update PartDist.py

--- synchmodel/PartDist.py
+++ synchmodel/PartDist.py
@@ -1,5 +1,7 @@
 import numpy as np
-def DistPL(norm,alpha,array):
+
+##-------- Funções para visualização do modelo de distribuição ------------##
+def DistPL_plot(norm,alpha,array):
     #This function calculates the particle distribution according to a PowerLaw
     #checking if parameters have the right types
     if not isinstance(norm,(float,int)):
@@ -15,7 +17,8 @@ def DistPL(norm,alpha,array):
     dist = norm*array**(-alpha)
     return dist
 
-def ExpCutPL(norm,alpha,array,beta,cut):
+
+def ExpCutPL_plot(norm,alpha,array,beta,cut):
     #checking if the parameters have the right type
     if not isinstance(norm,(float,int)):
         raise TypeError(f"'norm' must be float or int, received {type(norm).__name__}.")
@@ -33,3 +36,46 @@ def ExpCutPL(norm,alpha,array,beta,cut):
     #calculating PowerLaw distribution
     dist = norm*array**(-alpha)*np.exp((-array/cut)**(beta))
     return dist
+
+##-------- Funções para cálculo do modelo de distribuição e integração ------------##
+
+def PowerLaw(norm, alpha):
+
+    """
+    Creates a particle distribution function following a power-law.
+
+    Parameters:
+        norm (float): Normalization constant of the distribution.
+        alpha (float): Spectral index of the power law.
+
+    Returns:
+        function (callable): A function that computes the particle distribution for a given energy E.
+    """
+    #creating a function factory:
+    def distribution(E):
+        #calculating the distribution of particles according to a power law
+        if E <= 0:
+            raise ValueError("E must be positive.")
+        return norm * E**(-alpha)
+    return distribution
+
+def ExpCutOffPL(norm, alpha, beta, cut):
+    """
+    Creates a particle distribution function following an exponential cutoff power-law.
+
+    Parameters:
+        norm (float): Normalization constant of the distribution.
+        alpha (float): Spectral index of the power law.
+        beta (float): Exponential cutoff index.
+        cut (float): Cutoff energy scale.
+
+    Returns:
+        function (callable): A function that computes the particle distribution for a given energy E.
+    """
+    #creating a function factory:
+    def distribution(E):
+        #calculating the distribution of particle according to a exponencial cut off power law
+        if E <= 0:
+            raise ValueError("E must be positive.")
+        return norm * E**(-alpha) * np.exp(-(E / cut)**beta)
+    return distribution
