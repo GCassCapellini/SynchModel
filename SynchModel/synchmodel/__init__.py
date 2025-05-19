3a46d86de1f0c2aa3c5cb1a76353b089557af6c6
GCassCapellini <gabriel.capellini@usp.br>
2025-05-19 19:58:50 -0300

    Delete SynchModel directory

--- SynchModel/synchmodel/__init__.py
+++
@@ -1,6 +0,0 @@
-from .grid import grid
-from .integrate import integral
-from .PartDist import PowerLaw
-from .PartDist import ExpCutOffPL
-from .PartDist import ExpCutPL_plot
-from .PartDist import DistPL_plot
